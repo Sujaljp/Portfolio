742924befaaa806851c52ec548c57ae5c2002379
Sujal Jayashree Patil <sujalcode+github@gmail.com>
2023-09-14 19:15:52 +0530

    Portfolio changes initial

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -8,38 +8,63 @@ export const projectsData = [
     {
       id: 1,
       image: Work1,
-      title: "Web design",
-      category: "web",
+      title: "Land registry system: Hyperledger",
+      category: "blockchain",
+      link: "https://github.com/Sujaljp/Land-registry-using-Hyperledger-Fabric-"
     },
     {
       id: 2,
       image: Work2,
-      title: "App movil",
-      category: "app",
+      title: "Innovest",
+      category: "blockchain",
+      link: "https://github.com/Sujaljp/innovest"
     },
     {
       id: 3,
       image: Work3,
-      title: "Brand design",
-      category: "design",
+      title: "StudyBuddy",
+      category: "fullstack",
+      link: "https://github.com/Sujaljp/StudyBuddy"
     },
     {
       id: 4,
       image: Work4,
-      title: "App movil",
-      category: "app",
+      title: "Project Manager",
+      category: "frontend",
+      link: "https://github.com/Sujaljp/my-project-manager"
     },
     {
       id: 5,
       image: Work5,
-      title: "Web design",
-      category: "web",
+      title: "Housing society management",
+      category: "fullstack",
+    },
+    {
+      id: 6,
+      image: Work1,
+      title: "Bankwiz",
+      category: "frontend",
+      link:"https://github.com/Sujaljp/bankwiz"
+    },
+    {
+      id: 7,
+      image: Work2,
+      title: "Notes taker",
+      category: "frontend",
+      link:"https://github.com/Sujaljp/Notes-Taker-Vanila-JS-"
+    },
+    {
+      id: 8,
+      image: Work3,
+      title: "TextUtils",
+      category: "frontend",
+      link: "https://sujaljp.github.io/text-utils/"
     },
   ];
 
   export const projectsNav = [
     {name:'all'},
-    {name:'web'},
-    {name:'app'},
-    {name:'design'}
+    {name:'frontend'},
+    {name:'fullstack'},
+    {name:'blockchain'}
   ]
